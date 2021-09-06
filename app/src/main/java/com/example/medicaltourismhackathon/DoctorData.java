99a66966df85ec625923f227b2ac9dc86f0ecc55
Shubham Vig <shubhamgithub@outlook.com>
2021-09-07 03:15:16 +0530

    loginform

---
+++ app/src/main/java/com/example/medicaltourismhackathon/DoctorData.java
@@ -0,0 +1,8 @@
+package com.example.medicaltourismhackathon;
+
+public class DoctorData {
+    public  static final String[] doctorNames = {
+
+            "IND", "USA", "MY", "AFG", "MF"
+    };
+}
